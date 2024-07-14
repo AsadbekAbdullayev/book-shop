07587d02eb9b711e1fc507ce51ed28df8231f988
AsadbekAbdullayev <asadbekmmm127@gmail.com>
2024-07-14 12:33:07 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import bookRoute from "./routes/bookRoute.js";
+import cors from "cors";
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+app.use("/books", bookRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App conncted to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error, "error from mongose");
+  });
